e43f359cef95c21da7276c01a230b048704f589b
sharaaad <sharadpun@hotmail.co.uk>
2022-04-26 11:00:34 +0100

    Modified the ViewModel and Content View

--- CryptoCurrency/ContentView/ContentView.swift
+++ CryptoCurrency/ContentView/ContentView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var vm = ViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack{
+                VStack {
+                    Stepper("Amount: \(Int(vm.amount))€", value: $vm.amount, step: 100)
+                    Slider(value: $vm.amount, in: 1...10_000)
+                    
+                }
+                .padding()
+                
+                List(vm.filteredRates) { item in HStack() {
+                    Text(item.asset_id_quote)
+                        .bold()
+                    Spacer()
+                    Text("\(vm.calculateRate(rate: item), specifier: "%.2f")")
+                }
+                    
+                }
+                .listStyle(.plain)
+                .searchable(text: $vm.searchText)
+                
+            }
+            .onAppear(perform: vm.refereshData)
+            .navigationTitle("Crypto")
+            .toolbar {
+                ToolbarItem {
+                    Button("Refresh", action: vm.refereshData)
+                }
+            }
+        }
     }
 }
 
--- CryptoCurrency/ContentView/ContentViewModel.swift
+++ CryptoCurrency/ContentView/ContentViewModel.swift
@@ -9,3 +9,31 @@ import Foundation
 import SwiftUI
 
 
+extension ContentView {
+    class ViewModel: ObservableObject {
+        @Published var rates = [Rate]()
+        @Published var searchText = ""
+        @Published var amount: Double = 100
+        var filteredRates: [Rate] {
+            return searchText == "" ? rates : rates.filter {$0.asset_id_quote.contains(searchText.uppercased())}
+        }
+        
+        func calculateRate(rate: Rate) -> Double {
+            return amount * rate.rate
+        }
+        
+        func refereshData() {
+            CryptoAPI().getCryptoData(currency: "EUR", previewMode: false, { newRates in DispatchQueue.main.async {
+                withAnimation {
+                    self.rates = newRates
+                }
+                print("Successfully got new rates: \(self.rates.count)")
+                
+            }
+            })
+                                      
+        }
+        
+       
+    }
+}
